455db74af8499fa1ab8e293c76e36fee1e4e9fef
Avinashpotnuru <potnuruaavinash111@example.com>
2025-02-25 13:05:10 +0530

    add scroll to top button

--- src/components/Stories/index.jsx
+++ src/components/Stories/index.jsx
@@ -1,5 +1,3 @@
-import CountUp from "react-countup";
-
 import { Box } from "@mui/material";
 
 import StroiesBg from "/stories.webp";
@@ -7,17 +5,21 @@ import HeadingTitle from "../HeadingTitle";
 import { motion } from "framer-motion";
 
 import { variants } from "../../Animations";
+import { lazy, Suspense } from "react";
+
+const CountUp = lazy(() => import("react-countup"));
 
 const Stories = () => {
   const content = [
     { number: 18, title: " Years of Experience" },
     { number: 1200, title: "Join Indian Army" },
     { number: 100, title: "Best Teachers" },
-    { number: 5000, title: "Total Students" },];
+    { number: 5000, title: "Total Students" },
+  ];
   return (
     <Box className="relative flex flex-col justify-center items-center">
       <img
-        width="1200" 
+        width="1200"
         height="600"
         className="w-full h-auto"
         src={StroiesBg}
@@ -39,19 +41,21 @@ const Stories = () => {
           </h3>
           <Box className="grid grid-cols-2 gap-8 mt-8 ">
             {content.map((item, index) => (
-              <CountUp key={index} start={0} end={item.number} delay={0}>
-                {({ countUpRef }) => (
-                  <Box className="flex flex-col items-center text-center space-x-2 text-4xl font-bold text-[#F5961B]">
-                    <Box className="flex items-center">
-                      <span ref={countUpRef} />
-                      <span className="ml-1">+</span>{" "}
+              <Suspense key={index} fallback={<div>Loading...</div>}>
+                <CountUp start={0} end={item.number} delay={0}>
+                  {({ countUpRef }) => (
+                    <Box className="flex flex-col items-center text-center space-x-2 text-4xl font-bold text-[#F5961B]">
+                      <Box className="flex items-center">
+                        <span ref={countUpRef} />
+                        <span className="ml-1">+</span>{" "}
+                      </Box>
+                      <h1 className="text-sm text-black font-semibold !m-0">
+                        {item.title}
+                      </h1>
                     </Box>
-                    <h1 className="text-sm text-black font-semibold !m-0">
-                      {item.title}
-                    </h1>
-                  </Box>
-                )}
-              </CountUp>
+                  )}
+                </CountUp>
+              </Suspense>
             ))}
           </Box>
         </Box>
